b4e60fac46bbc40a002dc503e313cfde0aa79873
Ben Simon <ben@ideas2executables.com>
2014-12-15 07:36:22 -0500

    monitor light changes

--- src/com/benjisimon/lightjam/main.scm
+++ src/com/benjisimon/lightjam/main.scm
@@ -3,7 +3,27 @@
 ;;
 
 (require 'android-defs)
+
+(define-alias Sensor android.hardware.Sensor)
+(define-alias SensorManager android.hardware.SensorManager)
+(define-alias SensorEventListener android.hardware.SensorEventListener)
+(define-alias SensorEvent android.hardware.SensorEvent)
+
 (activity main
   (on-create-view
-   (android.widget.TextView (this)
-    text: "Hello, Android from Kawa Scheme!")))
+   (let* ((sensor-mgr (as SensorManager ((this):getSystemService "sensor")))
+          (light-sensor (sensor-mgr:getDefaultSensor Sensor:TYPE_LIGHT))
+          (feedback (android.widget.TextView (this) text: "--")))
+
+     (sensor-mgr:register-listener
+      (object (SensorEventListener)
+              ((on-accuracy-changed sensor accuracy) #!void)
+              ((on-sensor-changed (evt :: SensorEvent))
+               (feedback:setText (number->string (evt:values 0)))
+               #!void))
+      light-sensor
+      SensorManager:SENSOR_DELAY_FASTEST)
+      
+     feedback)))
+
+   
