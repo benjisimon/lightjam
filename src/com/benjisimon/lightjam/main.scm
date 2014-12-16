2ae1161a00405864ba0cf43e55f9e628c7f0a552
Ben Simon <ben@ideas2executables.com>
2014-12-16 08:02:40 -0500

    Not working yet, but got almost all the pieces in place

--- src/com/benjisimon/lightjam/main.scm
+++ src/com/benjisimon/lightjam/main.scm
@@ -13,11 +13,37 @@
 (define-alias AudioFormat android.media.AudioFormat)
 (define-alias Math java.lang.Math)
 (define-alias ByteVector gnu.lists.ByteVector)
+(define-alias Date java.util.Date)
 
 (define (logi . entries)
   (for-each (lambda (e) 
               (android.util.Log:i "com.benjisimon.lightjam" e))
             entries))
+
+
+
+(define (note)
+  (define (amplitude i)
+    (inexact->exact (floor (* (sin (/ (* 2 Math:PI i)
+                                      (/ 8000
+                                         440)))
+                              32767)))) 
+  (define (byteify x)
+    (let ((lower (bitwise-and x #x00FF))
+          (upper (bitwise-arithmetic-shift-right (bitwise-and x #xFF00) 8)))
+      ((as ByteVector (bytevector lower upper)):get-buffer)))
+
+  (let ((now ((Date):getTime)))
+    (byteify (amplitude (modulo now 100)))))
+
+
+(define (stringify x)
+  (let ((out (open-output-string)))
+    (display x out)
+    (get-output-string out)))
+
+
+
 
 (activity 
  main
@@ -32,7 +58,7 @@
                                      8000
                                      AudioFormat:CHANNEL_CONFIGURATION_MONO
                                      AudioFormat:ENCODING_PCM_16BIT
-                                     4
+                                     20
                                      AudioTrack:MODE_STREAM))
   (set! (this):sensor-mgr (as SensorManager ((this):getSystemService "sensor")))
 
@@ -42,13 +68,16 @@
  ((on-resume)
   (invoke-special android.app.Activity (this) 'onResume)
   (let* ((light-sensor ((this):sensor-mgr:getDefaultSensor Sensor:TYPE_LIGHT))
-         (feedback (this):feedback))
+         (feedback (this):feedback)
+         (track (this):track))
      (set! (this):handler 
            (object (SensorEventListener)
                    ((on-accuracy-changed sensor accuracy) #!void)
                    ((on-sensor-changed (evt :: SensorEvent))
-                    (let* ((light-level (evt:values 0)))
-                      (feedback:setText (string-append (number->string light-level)))
+                    (let* ((light-level (evt:values 0))
+                           (note        (note)))
+                      (feedback:setText (stringify (list light-level note)))
+                      (track:write note 0 1)
                       #!void))))
      ((this):sensor-mgr:register-listener (this):handler
                                    light-sensor
